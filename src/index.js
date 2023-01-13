483379a6c4ff3659ec35660f0eedd4206ed73a93
chakhoic <chakhoichan@gmail.com>
2023-01-13 14:55:30 -0500

    setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import Example from './scripts/example';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.querySelector('#root')
+    // console.log('hello world')
+
+    new Example(root);
+}) 
