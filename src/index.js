6fcd0251126f067fc3de1eade2a89ce234d0d320
chak <chakhoichan@gmail.com>
2023-03-16 15:40:39 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import { typeFilter } from "./scripts/typefilter.js";
+import { priceFilter } from "./scripts/pricefilter.js";
+import { partyFilter } from "./scripts/partyfilter.js";
+import { details } from "./scripts/details.js";
+
 window.onload = () => {
     const opts = document.querySelectorAll("select");
   
@@ -5,176 +10,10 @@ window.onload = () => {
         opts[i].addEventListener("change", () => {
           initMap();
         });
-      }
-  
-    const details = {
-        "Time Square": {
-          lat: 40.758896,
-          lng: -73.98513,
-          type: "Tourist",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "The High Line": {
-          lat: 40.747993,
-          lng: -74.00489,
-          type: "Tourist",
-          price: "$",
-          party: "Solo Advanture",
-          seasonal: "no",
-        },
-        "Hudson River Park": {
-          lat: 40.729563,
-          lng: -74.012699,
-          type: "Cultural",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "Empire State Building": {
-          lat: 40.748817,
-          lng: -73.985428,
-          type: "Tourist",
-          price: "$$",
-          party: "Solo Advanture",
-        },
-        "Koreatown": {
-          lat: 40.748817,
-          lng: -73.9755,
-          type: "Food/Drink",
-          price: "$$",
-          party: "Travel With Friends",
-        },
-        "Chinatown": {
-          lat: 40.7177,
-          lng: -74.0015,
-          type: "Food/Drink",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "Central Park": {
-          lat: 40.785091,
-          lng: -73.968285,
-          type: "Cultural",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "The Vessel": {
-          lat: 40.753826,
-          lng: -74.002228,
-          type: "Tourist",
-          price: "$$",
-          party: "Solo Advanture",
-        },
-        "Metropolitan Museum of Art": {
-          lat: 40.779434,
-          lng: -73.963402,
-          type: "Cultural",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "Museum of Modern Art": {
-          lat: 40.761509,
-          lng: -73.978271,
-          type: "Cultural",
-          price: "$",
-          party: "Solo Advanture",
-        },
-        "Chelsea Market": {
-          lat: 40.742352,
-          lng: -74.00621,
-          type: "Food/Drink",
-          price: "$$",
-          party: "Solo Advanture",
-        },
-        "St Marks Place": {
-          lat: 40.7285,
-          lng: -73.9882,
-          type: "Food/Drink",
-          price: "$$",
-          party: "Travel With Friends",
-        },
-        "Madison Square Garden": {
-          lat: 40.750298,
-          lng: -73.993324,
-          type: "Tourist",
-          price: "$$$",
-          party: "Travel With Friends",
-        },
-        "Rockefeller Center": {
-          lat: 40.758678,
-          lng: -73.978798,
-          type: "Tourist",
-          price: "$$",
-          party: "Solo Advanture",
-        },
-        "Little Italy": {
-          lat: 40.719141,
-          lng: -73.997327,
-          type: "Food/Drink",
-          price: "$$",
-          party: "Solo Advanture",
-        },
-        "Little Island": {
-            lat: 40.7421,
-            lng: -74.01,
-            type: "Tourist",
-            price: "$",
-            party: "Solo Advanture",
-          },
-          "SOHO": {
-            lat: 40.7246,
-            lng: -74.0019,
-            type: "Tourist",
-            price: "$$",
-            party: "Solo Advanture",
-          }
-        }          
-      
-
-  function partyFilter(obj, arr, value) {
-    if (value === "Default Party") {
-        return arr;
-    }
-    let filtered_array = []
-    for (let i = 0; i < arr.length; i++) {
-        if (obj[arr[i]]["party"] === value) {
-            filtered_array.push(arr[i])
-        }
-    }
-
-    return filtered_array
-}
-
-function priceFilter(obj, arr, value) {
-    if (value === "Default Price") {
-        return arr;
-    }
-    let filtered_array = []
-    for (let i = 0; i < arr.length; i++) {
-        if (obj[arr[i]]["price"] === value) {
-            filtered_array.push(arr[i])
-        }
-    }
-
-    return filtered_array
-}
-
-function typeFilter(obj, arr, value) {
-    if (value === "Default Type") {
-        return arr;
-    }
-    let filtered_array = []
-    for (let i = 0; i < arr.length; i++){
-        if (obj[arr[i]]["type"]===value) {
-            filtered_array.push(arr[i])   
-        }
-    }
-    return filtered_array
-}
-
+      }    
 
 function initMap() {
-    // Map initialization
+    // Map init
     window.map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: new google.maps.LatLng(40.776676, -73.971321),
@@ -195,7 +34,7 @@ function initMap() {
     displayMarkers(filtered3);
 
   
-    // Dropdown update code
+    // Dropdown update 
     const locationSelect = document.getElementById("location-select");
     locationSelect.addEventListener("change", () => {
       updateMarkersBasedOnSelectedLocation();
@@ -210,7 +49,7 @@ function initMap() {
       const selectedLocation = locationSelect.value;
     
       if (selectedLocation === "all") {
-        // Show all markers if the selected option is "----"
+        // Show all markers for "----"
         const party = document.getElementById("party");
         const party_size = party.options[party.selectedIndex].value;
         const type = document.getElementById("type");
@@ -231,16 +70,16 @@ function initMap() {
     
   
     if (!isLocationSelectEvent) {
-      // Clear the locationSelect dropdown
+      // clear dropdown
       locationSelect.innerHTML = '';
   
-      // Add an 'All Locations' option
+      // default option
       const allOption = document.createElement("option");
       allOption.value = "all";
       allOption.text = "----";
       locationSelect.add(allOption);
   
-      // Add the filtered locations
+      // filtered locations
       filtered3.forEach((loc) => {
         const option = document.createElement("option");
         option.value = loc;
@@ -248,18 +87,21 @@ function initMap() {
         locationSelect.add(option);
       });
     } else {
-      // Set the selected location in the dropdown
+      // set selected location in the dropdown
       locationSelect.value = selectedLocation;
     }
   
-    const deez = document.getElementById("deez");
-    deez.innerHTML = `Details: ${locationDetails.lat}, ${locationDetails.lng}, ${locationDetails.type}, ${locationDetails.price}, ${locationDetails.party}`;
+    const dee1 = document.getElementById("dee1");
+    dee1.innerHTML = `${locationDetails.about}`
+    
+    const dee2 = document.getElementById("dee2");
+    dee2.innerHTML = `${locationDetails.type}, ${locationDetails.price}, ${locationDetails.party};`
   
     
   }
 
   function displayMarkers(locations) {
-    // Clear existing markers before adding new ones
+    // clear existing markers before adding new ones
     if (window.markers) {
       for (let i = 0; i < window.markers.length; i++) {
         window.markers[i].setMap(null);
@@ -289,6 +131,5 @@ function initMap() {
     }
   }
   initMap();
-  // updateMarkersBasedOnSelectedLocation();
 
 };  
--- src/scripts/details.js
+++ src/scripts/details.js
@@ -1,105 +1,138 @@
 export const details = {
     "Time Square": {
-        "type": "Tourist",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.758896,
+      lng: -73.98513,
+      type: "Tourist",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"The Crossroads of the World"'
     },
-
     "The High Line": {
-        "type": "Tourist",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.747993,
+      lng: -74.00489,
+      type: "Tourist",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"Elevated urban park"'
     },
-
     "Hudson River Park": {
-        "type": "Cultural",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.729563,
+      lng: -74.012699,
+      type: "Cultural",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"Green park by the Hudson River"'
     },
-
     "Empire State Building": {
-        "type": "Tourist",
-        "price": "$$",
-        "party": "Solo Advanture",
+      lat: 40.748817,
+      lng: -73.985428,
+      type: "Tourist",
+      price: "$$",
+      party: "Solo Advanture",
+      about: '"Most iconic skyscraper in New York City"'
     },
-
     "Koreatown": {
-        "type": "Food/Drink",
-        "price": "$$",
-        "party": "Travel With Friends",
+      lat: 40.748817,
+      lng: -73.9755,
+      type: "Food/Drink",
+      price: "$$",
+      party: "Travel With Friends",
+      about: '"Korean food and culture hub"'
     },
-
     "Chinatown": {
-        "type": "Food/Drink",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.7177,
+      lng: -74.0015,
+      type: "Food/Drink",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"Authentic Chinese cuisine and street vendors"'
     },
-
     "Central Park": {
-        "type": "Cultural",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.785091,
+      lng: -73.968285,
+      type: "Cultural",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"Iconic urban park in New York City"'
     },
-
     "The Vessel": {
-        "type": "Tourist",
-        "price": "$$",
-        "party": "Solo Advanture",
+      lat: 40.753826,
+      lng: -74.002228,
+      type: "Tourist",
+      price: "$$",
+      party: "Solo Advanture",
+      about: '"Sculptural landmark with views in Hudson Yards"'
     },
-
     "Metropolitan Museum of Art": {
-        "type": "Cultural",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.779434,
+      lng: -73.963402,
+      type: "Cultural",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"Art museum with extensive collection and ancient artifacts"'
     },
-
     "Museum of Modern Art": {
-        "type": "Cultural",
-        "price": "$",
-        "party": "Solo Advanture",
+      lat: 40.761509,
+      lng: -73.978271,
+      type: "Cultural",
+      price: "$",
+      party: "Solo Advanture",
+      about: '"A world of avant-garde and contemporary art"'
     },
-
     "Chelsea Market": {
-        "type": "Food/Drink",
-        "price": "$$",
-        "party": "Solo Advanture",
+      lat: 40.742352,
+      lng: -74.00621,
+      type: "Food/Drink",
+      price: "$$",
+      party: "Solo Advanture",
+      about: '"Fresh seafood daily"'
     },
-
     "St Marks Place": {
-        "type": "Food/Drink",
-        "price": "$$",
-        "party": "Travel With Friends",
+      lat: 40.7285,
+      lng: -73.9882,
+      type: "Food/Drink",
+      price: "$$",
+      party: "Travel With Friends",
+      about: '"Bohemian street with unique bars and shops"'
     },
-
     "Madison Square Garden": {
-        "type": "Tourist",
-        "price": "$$$",
-        "party": "Travel With Friends",
+      lat: 40.750298,
+      lng: -73.993324,
+      type: "Tourist",
+      price: "$$$",
+      party: "Travel With Friends",
+      about: '"Home of the New York Knicks and New York Rangers"'
     },
-
-    "Rockefella Center": {
-        "type": "Tourist",
-        "price": "$$",
-        "party": "Solo Advanture",
+    "Rockefeller Center": {
+      lat: 40.758678,
+      lng: -73.978798,
+      type: "Tourist",
+      price: "$$",
+      party: "Solo Advanture",
+      about: '"Landmark in the heart of Midtown Manhattan"'
     },
-
     "Little Italy": {
-        "type": "Food/Drink",
-        "price": "$$",
-        "party": "Solo Advanture",
+      lat: 40.719141,
+      lng: -73.997327,
+      type: "Food/Drink",
+      price: "$$",
+      party: "Solo Advanture",
+      about: '"Charming Italian district with delicious food"'
     },
-
     "Little Island": {
-        "type": "Tourist",
-        "price": "$",
-        "party": "Solo Advanture",
-    },
-
-    "SOHO": {
-        "type": "Tourist",
-        "price": "$$",
-        "party": "Solo Advanture",
-    },
-
-
-}
+        lat: 40.7421,
+        lng: -74.01,
+        type: "Tourist",
+        price: "$",
+        party: "Solo Advanture",
+        about: '"Distinctive park on the hudson river"'
+      },
+      "SOHO": {
+        lat: 40.7246,
+        lng: -74.0019,
+        type: "Tourist",
+        price: "$$",
+        party: "Solo Advanture",          
+        about: '"Fashion capital of the world."'
+      }
+    }       
